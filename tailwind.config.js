da81bc159ecd2f5514eca86c816e1d17dc206fa2
baswanth papisetty <p.baslass@gmail.com>
2023-06-25 20:23:02 +0530

    initial

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,19 +7,9 @@ module.exports = {
       lg: "976px",
       xl: "1440px",
     },
-    borderRadius: {
-      'none': '0',
-      'sm': '10px',
-      DEFAULT: '16px',
-      'lg': '20px',
-      'full': '50px',
-    },
     extend: {
       colors: {
-        background: "#F4F4F4",
-        primary: "#344F68",
         white: "#F4F4F4",
-        secondart: "#322D2D",
       },
       dropShadow: {
         "3xl": "0 35px 35px rgba(0, 0, 0, 0.25)",
